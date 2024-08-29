eff68235abeddb1cdfc614df4e35e7f72cea7100
CHS Robotics <chsrobotics@cville.local>
2024-08-29 17:56:29 -0400

    skippy can pick up things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RAHHHHHHHHHHHHHHHHH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RAHHHHHHHHHHHHHHHHH.java
@@ -15,12 +15,16 @@ public class RAHHHHHHHHHHHHHHHHH extends OpMode {
     DcMotor br;
     Servo wrist;
     Servo claw;
+    DcMotor shaft;
+    DcMotor arm;
     @Override
     public void init() {
         fl = (DcMotor) hardwareMap.get("fl");
         fr = (DcMotor) hardwareMap.get("fr");
         br = (DcMotor) hardwareMap.get("br");
         bl = (DcMotor) hardwareMap.get("bl");
+
+        arm = (DcMotor) hardwareMap.get("arm");
 
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         fr.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -29,10 +33,12 @@ public class RAHHHHHHHHHHHHHHHHH extends OpMode {
 
         wrist = (Servo) hardwareMap.get("wrist");
         claw = (Servo) hardwareMap.get("claw");
+        shaft = (DcMotor) hardwareMap.get("shaft");
     }
 //the reason why this is here is just to set up //
     @Override
     public void loop() {
+        //pull down when using. It's like a plane//
         if(gamepad1.left_stick_y > 0) {
             fl.setPower(0.5);
             fr.setPower(0.5);
@@ -65,19 +71,54 @@ public class RAHHHHHHHHHHHHHHHHH extends OpMode {
 
         }
             else if(gamepad1.dpad_up){
-              wrist.setPosition(5);
+              wrist.setPosition(6);
 
         }
             else if(gamepad1.dpad_down){
 
                 claw.setPosition(1);
         }
+           //this is to close the claw//
+            else if(gamepad1.y){
+                claw.setPosition(0);
+
+        }
+
+            else if (gamepad1.right_stick_y < 0){
+
+                arm.setPower(1);
+
+
+
+        }
+
+
+            else if (gamepad1.right_stick_y > 0){
+
+                arm.setPower(-1);
+        }
+        else if (gamepad1.dpad_left){
+
+            shaft.setPower(-1);
+
+
+
+        }
+
+        else if (gamepad1.dpad_right){
+
+            shaft.setPower(1);
+        }
+
              else {
             fl.setPower(0);
             fr.setPower(0);
             bl.setPower(0);
             br.setPower(0);
+            arm.setPower(0);
+            shaft.setPower(0);
         }
+
 
 
     }
